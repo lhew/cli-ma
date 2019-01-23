0a51f67fdbafd03b3496e9f9587f158b268af983
Leonardo Almeida <leonardo.almeida@frontmen.nl>
2019-01-23 16:57:53 +0100

    correct path from bin

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const registerKey = require('./registerKey');
 const getWeather = require('./getWeather');
 const help = require('./help');
 
-try{
-    const args = parseArgs(process.argv.slice(2));
+module.exports = () => {
+    try{
+        const args = parseArgs(process.argv.slice(2));
 
-    if(args.key)
-        registerKey(args.key);
-    else if(args.city)
-        getWeather(args.city);
-    else if(args.help)
-        help();
-    else
-        help();
-    
+        if(args.key)
+            registerKey(args.key);
+        else if(args.city)
+            getWeather(args.city);
+        else if(args.help)
+            help();
+        else
+            help();
+        
 
-}catch(e){
-    console.log('wooow bro shit happened');
+    }catch(e){
+        console.log('wooow bro shit happened');
+    }
 }
